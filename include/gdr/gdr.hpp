d12415ee8c319a98e01f33efdc6a796cf140e2c6
maxnut <54683950+maxnut@users.noreply.github.com>
2024-03-08 16:30:12 +0100

    nvm i guess

--- include/gdr/gdr.hpp
+++ include/gdr/gdr.hpp
@@ -68,7 +68,6 @@ class Replay {
 	using InputType = T;
 	using Self = std::conditional_t<std::is_same_v<S, void>, Replay<S, T>, S>;
 
-	std::string name;
 	std::string author;
 	std::string description;
 
@@ -111,8 +110,6 @@ class Replay {
 			replayJson = json::parse(data);
 		}
 
-		if(replayJson.contains("name"))
-			replay.name = replayJson["name"];
 		replay.gameVersion = replayJson["gameVersion"];
 		replay.description = replayJson["description"];
 		replay.version = replayJson["version"];
@@ -149,7 +146,6 @@ class Replay {
 
 	std::vector<uint8_t> exportData(bool exportJson = false) {
 		json replayJson = saveExtension();
-		replayJson["name"] = name;
 		replayJson["gameVersion"] = gameVersion;
 		replayJson["description"] = description;
 		replayJson["version"] = version;
